1f463e7995fdf3799da3aba6e9de98549404b85b
Miguel Martinez <datos.sig02@conacyt.mx>
2021-10-18 16:28:13 -0500

    Agregando documentacion de clusteres

---
+++ docs/.vuepress/components/cluster/1-basico.vue
@@ -0,0 +1,35 @@
+<template>
+    <DaiTarjetaContenedorMapa>
+        <template #header>
+            Los clusteres son capas (casi siempre de tipo punto), que a medida que el zoom avanza se van desagregando
+            <DaiLeyendaMapa :para="['cluster-basico','sin-cluster']"/>
+        </template>
+        <DaiMapa>
+            <DaiCapaXyz />
+            <DaiCapaGeojson 
+            id="sin-cluster"
+            titulo="Capa sin cluster"
+            :url="$withBase('/centroides-estados.geojson')"
+            :estilo-capa="{
+                circle:{
+                    fill:{color:'white'},
+                    stroke:{color:'black',width:1},
+                    radius:4
+                }
+            }"
+            />
+            <DaiCapaGeojsonCluster 
+            id="cluster-basico"
+            titulo="Capa con cluster"
+            :url="$withBase('/centroides-estados.geojson')"
+            :estilo-capa="{
+                circle:{
+                    fill:{color:'red'},
+                    stroke:{color:'black',width:1},
+                    radius:6
+                }
+            }"
+            />
+        </DaiMapa>
+    </DaiTarjetaContenedorMapa>
+</template>
