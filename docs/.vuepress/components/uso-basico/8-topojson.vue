d132da57c35cca7aad5f5620cc2e3a78571ec9f7
Luis Colunga Perez <luis@karmapulse.com>
2022-07-27 15:37:08 -0500

    Agregar componente capa-topojson-layer

---
+++ docs/.vuepress/components/uso-basico/8-topojson.vue
@@ -0,0 +1,23 @@
+<template>
+    <dai-mapa 
+        :zoom="7" :centro="[-102.28259, 22.35981]"
+    >
+        <dai-capa-topojson 
+            id="disponibilidad" 
+            :url="$withBase('/topojson-sample.geojson')"
+            :reglas-estilo-capa="{
+                columna : 'classification',
+                acomodoCategorias: ['Con disponibilidad', 'Sin disponibilidad'],
+                propiedadObjetivo: 'relleno',
+                clases: 2,
+                colores: ['#00AFB9', '#F07167'],
+            }"
+            :estilo-capa="{
+                stroke: {
+                    color: '#fff',
+                    width: 0.2,
+                },
+            }"
+        />
+    </dai-mapa>
+</template>
