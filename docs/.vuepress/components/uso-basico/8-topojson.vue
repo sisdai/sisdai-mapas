bdc1dd3c93321f89e3f1c94f2789455655c6c094
Luis Colunga Perez <luis@karmapulse.com>
2022-07-28 18:02:13 -0500

    Agregar ejemplo de topojson con texturas

--- docs/.vuepress/components/uso-basico/8-topojson.vue
+++ docs/.vuepress/components/uso-basico/8-topojson.vue
@@ -1,22 +1,20 @@
 <template>
-    <dai-mapa 
-        :zoom="7" :centro="[-102.28259, 22.35981]"
-    >
-        <dai-capa-topojson 
-            id="disponibilidad" 
+    <dai-mapa :zoom="7" :centro="[-102.28259, 22.35981]">
+        <dai-capa-topojson
+            id="disponibilidad"
             :url="$withBase('/topojson-sample.geojson')"
             :reglas-estilo-capa="{
-                columna : 'classification',
+                columna: 'classification',
                 acomodoCategorias: ['Con disponibilidad', 'Sin disponibilidad'],
                 propiedadObjetivo: 'relleno',
                 clases: 2,
-                colores: ['#00AFB9', '#F07167'],
+                colores: ['#00AFB9', '#F07167']
             }"
             :estilo-capa="{
                 stroke: {
                     color: '#fff',
-                    width: 0.2,
-                },
+                    width: 0.2
+                }
             }"
         />
     </dai-mapa>
