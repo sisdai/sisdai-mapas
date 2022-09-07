b5ed2cb4b60da97a7dc2234c09126efd47e19597
andres-geotec <andres.geotec@gmail.com>
2022-09-07 17:56:45 -0500

    leyendo municipios del pais en topojson

--- docs/.vuepress/components/uso-basico/9-topojson-dinamico.vue
+++ docs/.vuepress/components/uso-basico/9-topojson-dinamico.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>hola</div>
+  <dai-mapa
+    :extension="[
+      -118.365119934082, 14.5320978164673, -86.7104034423828, 32.7186546325684,
+    ]"
+  >
+    <dai-capa-xyz-osm />
+
+    <dai-capa-topojson
+      :url="url"
+      :use-loader="true"
+    />
+  </dai-mapa>
 </template>
 
 <script>
 const url =
-  "https://gema.conacyt.mx/geoserver/contexto/wms?service=WMS&version=1.1.0&request=GetMap&layers=contexto%3Agref_division_estatal_2020&bbox=-118.36511993408203%2C14.532097816467285%2C-86.71040344238281%2C32.71865463256836&width=768&height=441&srs=EPSG%3A4326&styles=&format=application%2Fjson%3Btype%3Dtopojson";
+  "https://gema.conacyt.mx/geoserver/contexto/wms?service=WMS&version=1.1.0&request=GetMap&layers=contexto%3Agref_division_municipal_2020&bbox=-118.36511993408203%2C14.532097816467285%2C-86.71040344238281%2C32.71865463256836&width=768&height=441&srs=EPSG%3A4326&styles=&format=application%2Fjson%3Btype%3Dtopojson";
 
-export default {};
+export default {
+  computed: {
+    url() {
+      return url;
+    },
+  },
+};
 </script>
