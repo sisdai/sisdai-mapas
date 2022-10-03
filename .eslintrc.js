b28216c9a83a005ecb3b2a780b80cc302b42aff2
andres-geotec <andres.geotec@gmail.com>
2022-10-03 09:46:15 -0500

    eslintConfig en un archivo independiente

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ["plugin:vue/essential", "eslint:recommended"],
+    parserOptions: {
+        parser: "@babel/eslint-parser",
+    },
+    rules: {
+        "no-new": 0,
+        eqeqeq: "error",
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    },
+};
